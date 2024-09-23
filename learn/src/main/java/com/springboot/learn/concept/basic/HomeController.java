13b8dd38fa287e74c6af4c782c6cb1563c22697b
Sathyapriya T <sathya2001t@gmail.com>
2024-09-23 23:48:18 +0530

    Spring MVC - Front Controller

---
+++ learn/src/main/java/com/springboot/learn/concept/basic/HomeController.java
@@ -0,0 +1,17 @@
+package com.springboot.learn.concept.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+//spring can handle multiple controller at the same time - which all handled by front controller as a hidden layer
+@Controller
+public class HomeController {
+
+    @ResponseBody
+    @RequestMapping("/cart")
+    public String Cart(){
+        return "Your cart is empty";
+    }
+}
