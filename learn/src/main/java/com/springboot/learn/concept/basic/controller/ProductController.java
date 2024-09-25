30e91f215f18fe14ff8985141d9b42c36ddd4b6a
Sathyapriya T <sathya2001t@gmail.com>
2024-09-25 12:17:09 +0530

    CRUD operation using Data Map

--- learn/src/main/java/com/springboot/learn/concept/basic/controller/ProductController.java
+++ learn/src/main/java/com/springboot/learn/concept/basic/controller/ProductController.java
@@ -3,10 +3,16 @@ package com.springboot.learn.concept.basic.controller;
 import com.springboot.learn.concept.basic.data.Product;
 import com.springboot.learn.concept.basic.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ProductController {
@@ -14,8 +20,43 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
-    @RequestMapping("/products")
+    //RequestMapping also works the same as getMapping
+   /* @RequestMapping("/products")
     public List<Product> getProducts(){
         return productService.getProducts();
+    }*/
+    @GetMapping("/products")
+    public Map<Integer, Product> getProducts() {
+        return productService.getProducts();
+    }
+
+    @GetMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId) {
+        return productService.getProductById(prodId);
+    }
+
+    @PostMapping("/product")
+    public void saveProductById(@RequestBody Product product) {
+        productService.saveProduct(product);
+    }
+
+    @PostMapping("/products")
+    public void saveProducts(@RequestBody List<Product> products) {
+        productService.saveProducts(products);
+    }
+
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product product) {
+        productService.saveProduct(product);
+    }
+
+    @DeleteMapping("/products/{prodId}")
+    public void deleteProductById(@PathVariable int prodId) {
+        productService.deleteProduct(prodId);
+    }
+
+    @DeleteMapping("/products")
+    public void deleteProduct() {
+        productService.deleteProducts();
     }
 }
--- learn/src/main/java/com/springboot/learn/concept/basic/service/ProductService.java
+++ learn/src/main/java/com/springboot/learn/concept/basic/service/ProductService.java
@@ -3,15 +3,39 @@ package com.springboot.learn.concept.basic.service;
 import com.springboot.learn.concept.basic.data.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ProductService {
-    List<Product> products = List.of(new Product(101,"IPhone", 5000)
-            ,new Product(102,"WaterBottle", 50)
-            ,new Product(103,"Mug", 20));
+    private final Map<Integer, Product> productMap = new HashMap<>();
 
-    public List<Product> getProducts(){
-        return products;
+    public Map<Integer, Product> getProducts() {
+        return productMap;
     }
+
+    public Product getProductById(int prodId) {
+        return productMap.getOrDefault(prodId, null);
+    }
+
+    public void saveProduct(Product product) {
+        productMap.put(product.getProdId(), product);
+    }
+
+    public void deleteProduct(int prodId) {
+        if (getProductById(prodId) != null) {
+            productMap.remove(prodId);
+        }
+    }
+
+    public void saveProducts(List<Product> productList) {
+        productList.forEach(product -> productMap.put(product.getProdId(), product));
+
+    }
+    public void deleteProducts() {
+        productMap.clear();
+    }
+
+
 }
