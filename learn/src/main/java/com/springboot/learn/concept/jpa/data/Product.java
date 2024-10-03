f14c1eee68fe638ae8906400cdece2bb0e2b67ce
Sathyapriya T <sathya2001t@gmail.com>
2024-10-03 23:15:28 +0530

    @JsonFormat

--- learn/src/main/java/com/springboot/learn/concept/jpa/data/Product.java
+++ learn/src/main/java/com/springboot/learn/concept/jpa/data/Product.java
@@ -1,5 +1,6 @@
 package com.springboot.learn.concept.jpa.data;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +10,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Date;
 
 //When dealing with JPA repositories, we need make sure getters and setters are present in the Entity class. Or else the values will be saved as null.
 //If we are storing particular class as entity then it should have a primary which is annotated with @ID
@@ -28,4 +31,8 @@ public class Product {
     private String prodName;
 
     private int prize;
+
+    //This will convert the date to our convenient format
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private Date date;
 }
